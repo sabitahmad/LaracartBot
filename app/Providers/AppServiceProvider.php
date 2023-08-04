b7b2782fe73eea396826ff026bb8c5f8238ed66c
XCALIBUR <xcaliburdevelopment@gmail.com>
2023-08-04 20:25:33 +0600

    Changes: Filament Admin panel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
     }
     public function boot(): void
     {
+
+        Model::unguard(); //Filament only saves valid data to models so the models can be unguarded safely.
 
     }
 }
