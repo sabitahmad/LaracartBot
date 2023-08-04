9337bd8ef397e5b0e32e35aa4345319a44c3dca1
XCALIBUR <xcaliburdevelopment@gmail.com>
2023-08-04 20:32:39 +0600

    Add: ProductCategory.php and its schema/relation

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ProductCategory extends Model
+{
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, 'product_category_id');
+    }
+
+
+}
